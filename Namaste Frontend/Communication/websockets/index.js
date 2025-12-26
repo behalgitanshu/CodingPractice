849f8fc4e07ee9384c9a0f061a1c86e91330b978
Gitanshu Behal <behalgitanshu@gmail.com>
2025-12-26 23:15:50 +0530

    Web sockets example

---
+++ Namaste Frontend/Communication/websockets/index.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const { createServer } = require('node:http');
+const { join } = require('node:path');
+const { Server } = require('socket.io');
+
+const app = express();
+const server = createServer(app);
+const io = new Server(server);
+
+app.get('/', (req, res) => {
+  res.sendFile(join(__dirname, 'index.html'));
+});
+
+io.on('connection', (socket) => {
+  console.log('Connection established with client:', socket.id);
+
+  socket.on('chat message', (msg) => {
+    console.log('Message received:', msg);
+    io.emit('chat message', msg);
+  });
+
+  socket.on('disconnect', () => {
+    console.log('Client disconnected:', socket.id);
+  });
+});
+
+server.listen(3000, () => {
+  console.log('Server is listening on http://localhost:3000');
+});
+
